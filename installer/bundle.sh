ea8ee414570cd6ea62598ae3ba1af35384674db7
Justin Brooks <jsbroks@gmail.com>
2023-06-02 13:37:35 -0400

    clean up configs

--- installer/bundle.sh
+++ installer/bundle.sh
@@ -4,28 +4,8 @@ set -e
 
 DIR=.
 
-# <Config>
-export AIRGAP=0
-export ARCH="amd64"
-
-export KUBERNETES_VERSION="1.27.2"
-export CRICTL_VERSION="1.27.0"
-export CNI_PLUGINS_VERSION="1.3.0"
-export CONTAINERD_VERSION="1.7.1"
-export RUNC_VERSION="1.1.7"
-
-export OPENSSL_VERSION="3.1.1"
-
-export IMAGE_COREDNS=registry.k8s.io/coredns/coredns:v1.10.1
-export IMAGE_ETCD=registry.k8s.io/etcd:3.5.7-0
-export IMAGE_KUBE_API=registry.k8s.io/kube-apiserver:v$KUBERNETES_VERSION
-export IMAGE_KUBE_CONTROLLER=registry.k8s.io/kube-controller-manager:v$KUBERNETES_VERSION
-export IMAGE_KUBE_PROXY=registry.k8s.io/kube-proxy:v$KUBERNETES_VERSION
-export IMAGE_KUBE_SCHEDULER=registry.k8s.io/kube-scheduler:v1.27.2
-export IMAGE_PAUSE=registry.k8s.io/pause:3.9
-# </Config>
-
 # <ImportInline>
+. $DIR/installer/config.sh
 . $DIR/installer/common/kubernetes.sh
 . $DIR/installer/common/kubeadm.sh
 . $DIR/installer/common/logging.sh
@@ -36,11 +16,6 @@ export IMAGE_PAUSE=registry.k8s.io/pause:3.9
 . $DIR/installer/common/dependencies.sh
 . $DIR/installer/common/images.sh
 # </ImportInline>
-
-export PACKAGES=$DIR/packages/kubernetes/$KUBERNETES_VERSION
-export IMAGES=$DIR/packages/kubernetes/$KUBERNETES_VERSION/images
-export DEPENDENCIES=$DIR/packages/deps
-export HOSTNAME="$(hostname | tr '[:upper:]' '[:lower:]')"
 
 log_step "Generating installer"
 pnpm build:installer:airgap
