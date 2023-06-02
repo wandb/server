80b52513c18df8e6ca104de071a729db8895e557
Justin Brooks <jsbroks@gmail.com>
2023-06-01 21:42:27 -0400

    systemctl enable kubelet.service

--- installer/common/kubernetes.sh
+++ installer/common/kubernetes.sh
@@ -39,23 +39,7 @@ function kubernetes_install_packages() {
         # chmod a+rx /usr/bin/kubectl
     
         printf "Installing kubelet\n"
-        cat > "tmp-10-kubeadm.conf" <<EOF
-# Note: This dropin only works with kubeadm and kubelet v1.11+
-[Service]
-Environment="KUBELET_KUBECONFIG_ARGS=--bootstrap-kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf --kubeconfig=/etc/kubernetes/kubelet.conf"
-Environment="KUBELET_CONFIG_ARGS=--config=/var/lib/kubelet/config.yaml"
-# This is a file that "kubeadm init" and "kubeadm join" generates at runtime, populating the KUBELET_KUBEADM_ARGS variable dynamically
-EnvironmentFile=-/var/lib/kubelet/kubeadm-flags.env
-# This is a file that the user can use for overrides of the kubelet args as a last resort. Preferably, the user should use
-# the .NodeRegistration.KubeletExtraArgs object in the configuration files instead. KUBELET_EXTRA_ARGS should be sourced from this file.
-EnvironmentFile=-/etc/__ENV_LOCATION__/kubelet
-ExecStart=
-ExecStart=/usr/bin/kubelet \$KUBELET_KUBECONFIG_ARGS \$KUBELET_CONFIG_ARGS \$KUBELET_KUBEADM_ARGS \$KUBELET_EXTRA_ARGS
-EOF
-        mkdir -p /etc/systemd/system/kubelet.service.d
-        cp -f "tmp-10-kubeadm.conf" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
-        chmod 600 /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
-
+        kubernetes_configure_kubelet_systemd
         cp -f "$(package_filepath "kubelet")" /usr/bin/
         chmod a+rx /usr/bin/kubelet
     popd > /dev/null 2>&1
@@ -65,6 +49,49 @@ EOF
     systemctl enable kubelet && systemctl restart kubelet
 
     log_success "Kubernetes packages installed"
+}
+
+function kubernetes_configure_kubelet_systemd() {
+    # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+    
+    mkdir -p /etc/systemd/system/kubelet.service.d
+
+    cat > "kubelet.service" <<EOF [Unit]
+Description=kubelet: The Kubernetes Node Agent
+Documentation=https://kubernetes.io/docs/home/
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+ExecStart=/kubelet
+Restart=always
+StartLimitInterval=0
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target"
+EOF
+    cp -f "kubelet.service" "/etc/systemd/system/kubelet.service"
+    chmod 600 /etc/systemd/system/kubelet.service
+
+    cat > "10-kubeadm.conf" <<EOF
+# Note: This dropin only works with kubeadm and kubelet v1.11+
+[Service]
+Environment="KUBELET_KUBECONFIG_ARGS=--bootstrap-kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf --kubeconfig=/etc/kubernetes/kubelet.conf"
+Environment="KUBELET_CONFIG_ARGS=--config=/var/lib/kubelet/config.yaml"
+# This is a file that "kubeadm init" and "kubeadm join" generates at runtime, populating the KUBELET_KUBEADM_ARGS variable dynamically
+EnvironmentFile=-/var/lib/kubelet/kubeadm-flags.env
+# This is a file that the user can use for overrides of the kubelet args as a last resort. Preferably, the user should use
+# the .NodeRegistration.KubeletExtraArgs object in the configuration files instead. KUBELET_EXTRA_ARGS should be sourced from this file.
+EnvironmentFile=-/etc/default/kubelet
+ExecStart=
+ExecStart=/usr/bin/kubelet \$KUBELET_KUBECONFIG_ARGS \$KUBELET_CONFIG_ARGS \$KUBELET_KUBEADM_ARGS $KUBELET_EXTRA_ARGS
+EOF
+
+    mkdir -p /etc/systemd/system/kubelet.service.d
+    cp -f "10-kubeadm.conf" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+    chmod 600 /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
 }
 
 function kubernetes_has_packages() {
